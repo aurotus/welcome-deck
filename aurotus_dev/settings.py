0113df772b6535ce708b5cab1944dadd611785aa
Aquib Shahbaz <justaquib@Aquibs-MacBook-Air.local>
2026-04-22 00:31:03 +0530

    requriment added

--- aurotus_dev/settings.py
+++ aurotus_dev/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-td_5#yv%rb+dwmg!+az(mh6#s)vb$^s2+ejwn6hv-k-6*x+nd3'
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-td_5#yv%rb+dwmg!+az(mh6#s)vb$^s2+ejwn6hv-k-6*x+nd3')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
