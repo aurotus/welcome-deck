0c528bd52ea31ef155def058e6e7a3f46b12150e
Aquib Shahbaz <justaquib@Aquibs-MacBook-Air.local>
2026-04-22 00:51:32 +0530

    allow hosts

--- aurotus_dev/settings.py
+++ aurotus_dev/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-td_5#yv%rb+dwm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1', 'aurotus.com','.aurotus.com']
 
 
 # Application definition
